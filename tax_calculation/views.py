c476add109a3e2677dfce58454cfa6f6fc8402ed
Andrey Goryachev <raikslerx@gmail.com>
2022-10-24 19:57:01 +0300

    Добавлен рассчет специфической таможенной пошлины. Интегрирован API для получения текущего курса USD и EUR. Добавлена возможность задания собственного курса валют. Мелкие улучшения и правки, связаные с определением пошлины по адвалорной ставке.

--- tax_calculation/views.py
+++ tax_calculation/views.py
@@ -1,4 +1,6 @@
+import requests
 from django.shortcuts import render
+import re
 from tax_calculation.models import CustomTariff
 
 # Create your views here.
@@ -6,12 +8,16 @@ from tax_calculation.models import CustomTariff
 class Ett_handler:
     def __init__(self, request):
         self.code = request.POST.get('code')
-        self.mass = request.POST.get('mass')
+        self.specific_metric = request.POST.get('specific_metric')
+        self.specific_label = request.POST.get('specific_label')
+        self.specific_valute = request.POST.get('specific_valute')
+        self.cource = request.POST.get('cource')
+        self.usertype_cource = request.POST.get('usertype_cource')
         self.price = request.POST.get('price')
         self.tax = CustomTariff.objects.get(tnved_code=self.code).tax
 
-    def get_parameters(self):
-        return {'code' : self.code, 'mass' : self.mass, 'price' : self.price, 'tax' : self.tax}
+    def get_parameters(self):  # Возврат контекста для шаблонов.
+        return {'code' : self.code, 'specific_metric' : self.specific_metric, 'specific_label' : self.specific_label, 'specific_valute' : self.specific_valute,'price' : self.price, 'tax' : self.tax, 'cource' : self.cource, 'usertype_cource' : self.usertype_cource}
 
     def tax_type_checker(self):
         if (len(self.tax) <= 3 and self.tax[-1] == "%") or self.tax == "0":
@@ -22,11 +28,43 @@ class Ett_handler:
             self.tax_type = 'combine'
         return self.tax_type
 
-    def calculate_tax(self):
+    def get_specific_label(self):
+        return re.search(r'(?<=\/).*', self.tax).group(0)
+    
+    def get_specific_tax(self):
+        return re.search(r'.{1,}\d', self.tax).group(0)
+
+    def get_specific_valute(self):
+        if 'евро' in self.tax:
+            return 'евро'
+        else:
+            return 'долл'
+
+    def get_cource(self, valute):
+        if valute == 'долл':
+            cource = requests.get('https://api.coingate.com/v2/rates/merchant/USD/RUB').json()
+            return cource
+        if valute == 'евро':
+            cource = requests.get('https://api.coingate.com/v2/rates/merchant/EUR/RUB').json()
+            return cource
+
+    def calculate_tax(self, specific_metric=None):
         tax_type = self.tax_type_checker()
         if tax_type == 'advalore':
-            tax = int(self.price) * (int(self.tax[:-1]) / 100)
-            return tax
+            if self.tax != '0':
+                tax = int(self.price) * (int(self.tax[:-1]) / 100)
+                return round(tax, 2)
+            else:
+                return 0
+        elif tax_type == 'specific':
+            if self.usertype_cource == '':  # Если не введен кастомный курс валюты
+                specific_tax = self.get_specific_tax()
+                tax = float(specific_tax) * int(specific_metric) * float(self.cource)
+            else:
+                specific_tax = self.get_specific_tax()
+                tax = float(specific_tax) * int(specific_metric) * float(self.usertype_cource)
+            return round(tax, 2)
+
 
 
 def index(request):
@@ -39,7 +77,6 @@ def calculate_tax(request):
     parameters['tax_type'] = tax_type
     #Проверка на достаточность данных для рассчета по адвалорной формуле. Если данных недостаточно, запрашиваем дополнительные, если достаточно, производим рассчет.
     if tax_type == 'advalore':
-        print(parameters['price'])
         if parameters['price'] == '':
             return render(request, 'result.html', context=parameters)
         else:
@@ -48,7 +85,15 @@ def calculate_tax(request):
             return render(request, 'result.html', context=parameters)
     #Проверка на достаточность данных для рассчета по специфической формуле. Если данных недостаточно, запрашиваем дополнительные если достаточно, производим рассчет.
     elif tax_type == 'specific':
-        if parameters['mass'] == '':
+        if parameters['specific_metric'] == '':
+            parameters['specific_label'] = handler.get_specific_label()
+            parameters['specific_valute'] = handler.get_specific_valute()
+            parameters['cource'] = handler.get_cource(valute=parameters['specific_valute'])
+            return render(request, 'result.html', context=parameters)
+        else:
+            calculated_tax = handler.calculate_tax(parameters['specific_metric'])
+            parameters['calculated_tax'] = calculated_tax
+            print(parameters)
             return render(request, 'result.html', context=parameters)
     #Проверка на достаточность данных для рассчета по комбинированной формуле. Если данных недостаточно, запрашиваем дополнительные если достаточно, производим рассчет.
     elif tax_type == 'combine':
